9907714406b86d8e28685117960d1c6399c48cba
Brian Fogg <fogg4444@gmail.com>
2018-07-04 11:11:54 -0400

    remove test data

--- app/mobxStores/Domain/RegistrationStore.js
+++ app/mobxStores/Domain/RegistrationStore.js
@@ -48,7 +48,7 @@ export default class RegistrationStore {
             password: '',
         }
         this.initRegistation()
-        this.initWithTestData()
+        // this.initWithTestData()
     }
 
     @action initWithTestData = () => {
