0ba2ba90b30e8e6ae3e2f2dee44463eb7a57b457
Brian Fogg <fogg4444@gmail.com>
2018-07-04 19:55:17 -0400

    fix registration start page

--- app/routes/Registration/index.js
+++ app/routes/Registration/index.js
@@ -57,7 +57,7 @@ class RegistrationPage extends React.Component {
     this.state = { 
       email: '', 
       behavior: 'padding', 
-      step: 4,
+      step: 0,
       colors: colors(props.globalData.isDarkThemeActive)
     };
   }
