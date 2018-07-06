68144191aa912887cb0169d38e4857ac83ab58fd
Brian Fogg <fogg4444@gmail.com>
2018-07-05 22:20:02 -0400

    attempting fix to funding transition flow

--- app/routes/appnav.js
+++ app/routes/appnav.js
@@ -79,7 +79,6 @@ const StackNav = StackNavigator({
   Success: {
     screen: Success
   },
-
 }, {
   initialRouteName: stackNavDefaultRoute,
   lazy: false,
@@ -87,24 +86,11 @@ const StackNav = StackNavigator({
   mode: 'modal',
   headerMode: 'float',
   headerTransitionPreset: 'uikit',
-
-  screenInterpolator: sceneProps => {
-    const { layout, position, scene } = sceneProps;
-    const { index } = scene;
-
-    const height = layout.initHeight;
-    const translateY = position.interpolate({
-      inputRange: [index - 1, index, index + 1],
-      outputRange: [height, 0, 0],
-    });
-
-    const opacity = position.interpolate({
-      inputRange: [index - 1, index - 0.99, index],
-      outputRange: [0, .1, 1],
-    });
-
-    return { opacity, transform: [{ translateY }] };
-  }
+  transitionConfig: () => ({
+    transitionSpec: {
+      duration: 0,
+    },
+  }),
 });
 
 
