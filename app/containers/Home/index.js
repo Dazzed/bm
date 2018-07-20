11e2476a13bfffc1fa294fcc8b955d1595b3068b
Dazzed <sameepsheth@gmail.com>
2018-07-20 20:54:37 +0530

    Build 34

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -158,7 +158,7 @@ class HomeScreen extends Component {
           onPress={() => navigate('Login')}>
           <Text style={[{ color: this.state.colors['realWhite'] }, styles.fullBtnTxt, fonts.hindGunturBd]}>ALREADY A MEMBER? SIGN IN</Text>
         </TouchableHighlight>
-        <Text>Version: 0.0.1 (33)</Text>
+        <Text>Version: 0.0.1 (34)</Text>
         <Modal
           isVisible={this.state.isTermsVisible}
           animationIn={'slideInUp'}
