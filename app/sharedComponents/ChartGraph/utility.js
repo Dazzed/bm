1c301e151bed04427a424e6bb1d9bb9bc4f6dddf
Brian Fogg <fogg4444@gmail.com>
2018-08-16 12:02:05 -0700

    prevent zeros from skewing ichi cloud

--- app/sharedComponents/ChartGraph/utility.js
+++ app/sharedComponents/ChartGraph/utility.js
@@ -270,12 +270,49 @@ export const parseSmallGraphData = (data, Price, graphHeight, range) => {
 
 export const parseLargeGraphData = (inputData, height, width, indicatorsList, theme, range) => {
 
+
+    let modifiedInputData = inputData;
+
+
+    // limit input data if in one hour mode
+    if(range === '1h') {
+      let formattedDataPointsHour = [];
+
+      console.log('-========================== LIMIT INPUT DATA', inputData);
+      let currentTime = moment().format('X');
+      let oneHourAgo = moment().subtract(1, 'hours').format('X');
+
+      console.log('current time', currentTime);
+      console.log('one hour ago', oneHourAgo);
+
+
+      inputData.reverse().every((elem, i) => {
+
+        let dateUnix = returnFormattedTimeStamp(elem);
+        console.log('each data unix', dateUnix)
+        if(dateUnix > oneHourAgo) {
+          formattedDataPointsHour.push(elem);
+          return true;
+        } else {
+          return false;
+        }
+      })
+
+
+      formattedDataPointsHour = formattedDataPointsHour.reverse();
+
+      console.log('=== formattedDataPointsHour', formattedDataPointsHour, formattedDataPointsHour.length)
+
+      modifiedInputData = formattedDataPointsHour;
+    }
+
+
     let d = {
       xMax: 0,
       xMin: 99999999999999999999999,
       yMax: 0,
       yMin: 99999999999999999999999,
-      dataPoints: inputData,
+      dataPoints: modifiedInputData,
       gridYArray: [],
       gridXArray: [],
       xLineCount: 5,
@@ -307,7 +344,10 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
       if( input < d.xMin ) d.xMin = input;
     }
 
-    const manipulateYMaxMin = (input) => {
+    const manipulateYMaxMin = (input, ignoreZero) => {
+      if(input === 0 && ignoreZero) {
+        return;
+      }
       if( input > d.yMax ) d.yMax = input;
       if( input < d.yMin ) d.yMin = input;
     }
@@ -455,8 +495,8 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
       if(renderIchi) {
         manipulateYMaxMin(elem.ichi.base);
         manipulateYMaxMin(elem.ichi.conversion);
-        manipulateYMaxMin(elem.ichi.spanA);
-        manipulateYMaxMin(elem.ichi.spanB);
+        manipulateYMaxMin(elem.ichi.spanA, true);
+        manipulateYMaxMin(elem.ichi.spanB, true);
       }
 
       // conditional rendering based on indicators menu
@@ -546,14 +586,17 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
 
     const generateLineData = (targetValue, color) => {
       let lineData = [];
-      d.dataPoints.forEach((elem, i) => {
+
+
+      for (let elem of d.dataPoints) {
         let chosenValue = Object.byString(elem, targetValue);
         let xRel = (elem.dateUnix - d.xMin) / (d.xRange);
         let xCoord = xRel * width;
         let yRel = (chosenValue - d.yMin) / d.yRange;
         let yCoord = flipYAxisValue(height, yRel * height);
         lineData.push(`${xCoord},${yCoord}`)
-      })
+      }
+
       let formattedLineData = lineData.join(' ')
       return {
         color: color,
@@ -564,7 +607,8 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
 
     const generateRelativeLineData = (targetValue, color, max, min) => {
       let lineData = [];
-      d.dataPoints.forEach((elem, i) => {
+
+      for (let elem of d.dataPoints) {
         let chosenValue = Object.byString(elem, targetValue);
         let xRel = (elem.dateUnix - d.xMin) / (d.xRange);
         let xCoord = xRel * width;
@@ -572,7 +616,7 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
         let graphYPosition = yRelative * height;
         let yCoord = flipYAxisValue(height, graphYPosition);
         lineData.push(`${xCoord},${yCoord}`)
-      })
+      }
 
       let formattedLineData = lineData.join(' ')
       return {
@@ -584,7 +628,7 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
 
     const generateVolumeBottomLinesData = (targetValue) => {
       let lineData = [];
-      d.dataPoints.forEach((elem, i) => {
+      for ( let elem of d.dataPoints) {
         let volumeValue = Object.byString(elem, targetValue);
         let openValue = Object.byString(elem, 'open');
         let closeValue = Object.byString(elem, 'close');
@@ -606,7 +650,7 @@ export const parseLargeGraphData = (inputData, height, width, indicatorsList, th
           yCoord: yCoord,
           xCoord: xCoord
         })
-      })
+      }
       return {
         dataSet: lineData
       }
